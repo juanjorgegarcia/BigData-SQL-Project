2c9d4eb4fcfe3bb410f2e53d16cce9d0d29d48a0
juanjorgegarcia <juan.j.garcia007@gmail.com>
2019-10-20 18:44:47 -0300

    adicioanndo testes e api rest

--- projeto.py
+++ projeto.py
@@ -33,17 +33,22 @@ def update_person_username(conn, person_id, value):
 
 
 def remove_person(conn, person_id):
+
     with conn.cursor() as cursor:
-        cursor.execute(
-            'update person SET is_deleted = 1 where person_id=%s', (person_id))
+        try:
+            cursor.execute(
+                'UPDATE person SET is_deleted = 1 WHERE person_id=%s', (person_id))
+        except pymysql.err.IntegrityError as e:
+            print(e)
+            raise ValueError(
+                f'Não posso remover a pessoa com o id{person_id} na tabela person')
 
 
 def list_persons(conn):
     with conn.cursor() as cursor:
-        cursor.execute('SELECT person_id from person WHERE is_deleted =0')
+        cursor.execute('SELECT * from person WHERE is_deleted = 0')
         res = cursor.fetchall()
-        persons = tuple(x[0] for x in res)
-        return persons
+        return res
 
 
 def add_bird(conn, name):
@@ -129,6 +134,13 @@ def find_post(conn, post_id):
             return None
 
 
+def list_posts(conn):
+    with conn.cursor() as cursor:
+        cursor.execute('SELECT * from post WHERE deletedAt is NULL')
+        res = cursor.fetchall()
+        return res
+
+
 def find_post_id(conn, post_name):
     with conn.cursor() as cursor:
         cursor.execute(
@@ -171,7 +183,7 @@ def update_post_content(conn, post_id, value):
     with conn.cursor() as cursor:
         try:
             cursor.execute(
-                'UPDATE post SET content=%s where post_id=%s', (value, post_id))
+                'UPDATE post SET content=%s WHERE post_id=%s', (value, post_id))
         except pymysql.err.IntegrityError as e:
             raise ValueError(
                 f'Não posso alterar a propriedade content do id {post_id} para {value} na tabela post')
@@ -229,3 +241,133 @@ def find_view(conn, person_id, post_id):
             return res[0]
         else:
             return None
+
+
+def add_post(conn, title, url, content, person_id):
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute('INSERT INTO post (title, url, content, person_id) VALUES (%s,%s,%s,%s)',
+                           (title, url, content, person_id))
+        except pymysql.err.IntegrityError as e:
+            raise ValueError(
+                f'Não posso inserir {title, url, content} na tabela post')
+
+
+def add_post_refere_person(conn, post_id, person_id):
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute('INSERT INTO post_refere_person (post_id ,person_id) VALUES (%s,%s)',
+                           (post_id, person_id))
+        except pymysql.err.IntegrityError as e:
+            raise ValueError(
+                f'Não posso inserir {post_id, person_id} na tabela post_refere_person')
+
+
+def remove_post_refere_person(conn, post_id, person_id):
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute('UPDATE post_refere_person SET deletedAt = CURDATE(), WHERE post_id = %s and person_id = %s ',
+                           (post_id, person_id))
+        except pymysql.err.IntegrityError as e:
+            raise ValueError(
+                f'Não posso remover a referencia: {post_id, person_id} na tabela post_refere_person')
+
+
+def list_all_references_person(conn, person_id):
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute('SELECT post_id, person_id FROM post_refere_person WHERE deletedAt is NULL and person_id = %s ',
+                           (person_id))
+        except pymysql.err.IntegrityError as e:
+            raise ValueError(
+                f'Não posso listar todas as referencias da pessoa: {person_id} na tabela post_refere_person')
+
+
+def list_all_references_post(conn, post_id):
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute('SELECT post_id, person_id FROM post_refere_person WHERE deletedAt is NULL and post_id = %s ',
+                           (post_id))
+        except pymysql.err.IntegrityError as e:
+            raise ValueError(
+                f'Não posso listar todas as referencias do post: {post_id} na tabela post_refere_person')
+
+
+def add_person_comment_post(conn, post_id, person_id, comment):
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute('INSERT INTO person_comment_post (post_id ,person_id, comment) VALUES (%s,%s,%s)',
+                           (post_id, person_id, comment))
+        except pymysql.err.IntegrityError as e:
+            raise ValueError(
+                f'Não posso inserir {post_id, person_id, comment} na tabela person_comment_person')
+
+
+def remove_person_comment_post(conn, post_id, person_id):
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute('UPDATE person_comment_post SET deletedAt = CURDATE(), WHERE post_id = %s and person_id = %s ',
+                           (post_id, person_id))
+        except pymysql.err.IntegrityError as e:
+            raise ValueError(
+                f'Não posso remover o comentario: {post_id, person_id} na tabela person_comment_post')
+
+
+def list_all_comments_of_person(conn, person_id):
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute('SELECT * FROM person_comment_post WHERE deletedAt is NULL and person_id = %s ',
+                           (person_id))
+        except pymysql.err.IntegrityError as e:
+            raise ValueError(
+                f'Não posso listar todas os comentarios da pessoa: {person_id} na tabela person_comment_post')
+
+
+def list_all_comments_of_post(conn, post_id):
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute('SELECT * FROM person_comment_post WHERE deletedAt is NULL and post_id = %s ',
+                           (post_id))
+        except pymysql.err.IntegrityError as e:
+            raise ValueError(
+                f'Não posso listar todas os comentarios do post: {post_id} na tabela person_comment_post')
+
+
+def add_post_refere_bird(conn, post_id, bird_name):
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute('INSERT INTO post_refere_bird (post_id, bird_name) VALUES (%s,%s)',
+                           (post_id, bird_name))
+        except pymysql.err.IntegrityError as e:
+            raise ValueError(
+                f'Não posso inserir {post_id, bird_name} na tabela post_refere_bird')
+
+
+def remove_post_refere_bird(conn, post_id, bird_name):
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute('UPDATE post_refere_bird SET deletedAt = CURDATE(), WHERE post_id = %s and bird_name = %s ',
+                           (post_id, bird_name))
+        except pymysql.err.IntegrityError as e:
+            raise ValueError(
+                f'Não posso remover a referencia: {post_id, bird_name} na tabela post_refere_bird')
+
+
+def list_all_post_references_of_bird(conn, post_id, bird_name):
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute('SELECT post_id, bird_name FROM post_refere_bird WHERE deletedAt is NULL and bird_name = %s ',
+                           (bird_name))
+        except pymysql.err.IntegrityError as e:
+            raise ValueError(
+                f'Não posso listar todas as referencias do passaro: {bird_name} na tabela post_refere_bird')
+
+
+def list_all_bird_references_of_post(conn, post_id):
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute('SELECT post_id, bird_name FROM post_refere_bird WHERE deletedAt is NULL and post_id = %s ',
+                           (post_id))
+        except pymysql.err.IntegrityError as e:
+            raise ValueError(
+                f'Não posso listar todas os passaros do post: {post_id} na tabela post_refere_bird')
